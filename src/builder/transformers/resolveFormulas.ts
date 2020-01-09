eda293e5de1d5f6ba1e7d6749861d5e4e2eae21d
Noam Rosenthal <noam.j.rosenthal@gmail.com>
2020-01-09 23:43:22 +0200

    rewriteFormula

--- src/builder/transformers/resolveFormulas.ts
+++ src/builder/transformers/resolveFormulas.ts
@@ -4,8 +4,8 @@ import path from 'path'
 import { Slot } from '../StoreDefinition'
 import {F, P, R, removeUndefined, S} from './helpers'
 import useMacro from './useMacro'
-import { PrimitiveFormula, toArgType, TypedPrimitive } from '../types'
-import { TypedFormula, Bundle, Formula, TransformData, NativeType,
+import { PrimitiveFormula, toArgType, TypedPrimitive, 
+        TypedFormula, Bundle, Formula, TransformData, NativeType,
          FunctionFormula, SlotStatement, ReferenceFormula, toFormula } from '../types'
 
 const nativeFunctions = new Set([
@@ -44,6 +44,8 @@ const functions: {[name: string]: (...args: any[]) => Formula} = {
 }
 
 function formulaToString(f: Formula): string {
+    if (!f)
+        return ''
     const rf = f as ReferenceFormula
     const pf = f as PrimitiveFormula
     const ff = f as FunctionFormula
@@ -53,13 +55,104 @@ function formulaToString(f: Formula): string {
     if (Reflect.has(pf, '$primitive'))
         return '' + pf.$primitive
 
-    if (ff.op)
-        return `${ff.op}(${(ff.args || []).map(formulaToString).join(',')})`
+    if (ff.op) {
+        const args = ff.args || []
+        const T = (tsa: TemplateStringsArray, ...A: number[]) => 
+            tsa.map((str, i) => `${str}${formulaToString(args[A[i]])}`).join('')
+
+        switch (ff.op) {
+            case 'not':
+                return T`!${0}`
+            case 'get':
+                return T`${0}[${1}]`
+            case 'bwand':
+                return T`(${0} & ${1})`
+            case 'eq':
+                return T`(${0} === ${1})`
+            case 'neq':
+                return T`(${0} !== ${1})`
+            case 'plus':
+                return T`(${0} + ${1})`
+            case 'minus':
+                return T`(${0} - ${1})`
+            case 'mult':
+                return T`(${0} * ${1})`
+            case 'div':
+                return T`(${0} / ${1})`
+            case 'pow':
+                return T`(${0} ** ${1})`
+            case 'lte':
+                return T`(${0} <= ${1})`
+            case 'gte':
+                return T`(${0} >= ${1})`
+            case 'shr':
+                return T`(${0} >> ${1})`
+            case 'shl':
+                return T`(${0} << ${1})`
+            case 'bwor':
+                return T`(${0} | ${1})`
+            case 'cond':
+                return T`(${0} ? ${1} : ${2})`
+            case 'or':
+                return args.map(formulaToString).join(' || ')
+            case 'and':
+                return args.map(formulaToString).join(' && ')
+            case 'array':
+            case 'pair':
+                return `[${args.map(formulaToString).join(', ')}]`
+            case 'cond':
+                return T`(${0} ? ${1} : ${2})`
+        }
+        return `${ff.op}(${(args || []).map(formulaToString).join(',')})`
+    }
 
     return JSON.stringify(ff)
 
 }
+
+const unknownSymbol = Symbol('unknown')        
 export default function resolveFormulas(bundle: Bundle, im: TransformData): Bundle {
+    const isTruthy = (f: Formula): boolean|Symbol => {
+        if (Reflect.has(f, '$primitive'))
+            return !!Reflect.get(f, '$primitive')
+        const {op, args} = f as FunctionFormula
+        const newArgs = (args || []).map(a => tryRewrite(a, a) || a)        
+        if (new Set(['array', 'object', 'pair']).has(op))
+            return true
+
+        if (op === 'not') {
+            const v = isTruthy((newArgs || [])[0])
+            if (v !== unknownSymbol)
+                return !v
+        }
+
+        return unknownSymbol
+    }
+    
+    const tryRewrite = (f: Formula, d?: Formula|undefined): Formula|undefined => {
+        if (!Reflect.has(f, 'op'))
+            return d
+        const {op, args} = f as FunctionFormula
+        const a = args || []
+        if (op === 'cond') {
+            const truthy = isTruthy(a[0])
+            if (truthy === unknownSymbol)
+                return d
+
+            return truthy ? resolveFormula(a[1]) : resolveFormula(a[2])
+        }
+
+        if (op === 'not') {
+            const truthy = isTruthy(a[0])
+            if (truthy === unknownSymbol)
+                return d
+
+            return {$primitive: !truthy} as Formula
+        }
+
+        return d
+    }
+
     const refs = {
         ...bundle.filter(s => s.type === 'Slot')
             .map(s => ({[s.name as string]: (s as SlotStatement).formula})).reduce(assign, {}),
@@ -85,6 +178,11 @@ export default function resolveFormulas(bundle: Bundle, im: TransformData): Bund
             return {op: f.length === 2 ? 'pair': 'array', args: (f as any[]).map(resolveFormula)} as Formula
 
         const {op, args} = f as FunctionFormula
+        const rewritten = tryRewrite(f)
+        if (rewritten) {
+            console.log({rewritten})
+            return rewritten
+        }
 
         if (functions[op])
             return resolveFormula(functions[op](...(args || [])) as FunctionFormula)
@@ -97,9 +195,7 @@ export default function resolveFormulas(bundle: Bundle, im: TransformData): Bund
 
     im.roots = mapValues(im.roots, resolveFormula)
     im.debugInfo = {
-        roots: mapValues(im.roots, formulaToString),
-        slots: bundle.filter(({type}) => type === 'Slot').map(f => 
-            ({[f.name || '']: formulaToString((f as SlotStatement).formula)})).reduce(assign)
+        roots: mapValues(im.roots, formulaToString)
     }
 
     console.log(im.debugInfo)
